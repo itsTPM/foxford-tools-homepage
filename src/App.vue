fbe58a93d792419d30479f1b3e2c09fe4acc2035
itsTPM <26815728+itsTPM@users.noreply.github.com>
2026-05-22 03:14:06 +0300

    Migrate from Tailwind CSS to Scoped CSS

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,92 @@
 <script setup>
-import Button from "./components/ui/Button.vue";
+import Link from './components/ui/Link.vue';
 
 const mainLinks = [
   {
-    subtitle: "установить для",
-    title: "Chrome",
-    iconSrc: "/icons/chrome.svg",
-    link: "https://chromewebstore.google.com/detail/foxford-tools/mmhgkmkmoepfpcakdkajpendcnjichhm",
+    subtitle: 'установить для',
+    title: 'Chrome',
+    iconSrc: '/icons/chrome.svg',
+    link: 'https://chromewebstore.google.com/detail/foxford-tools/mmhgkmkmoepfpcakdkajpendcnjichhm',
   },
   {
-    subtitle: "код на",
-    title: "GitHub",
-    iconSrc: "/icons/github.svg",
-    link: "https://github.com/itsTPM/foxford-tools",
+    subtitle: 'код на',
+    title: 'GitHub',
+    iconSrc: '/icons/github.svg',
+    link: 'https://github.com/itsTPM/foxford-tools',
   },
   {
-    title: "Справка",
-    iconSrc: "/icons/docs.svg",
-    link: "https://foxwiki.itstpm.rocks",
+    title: 'Справка',
+    iconSrc: '/icons/docs.svg',
+    link: 'https://foxwiki.itstpm.rocks',
   },
 ];
 </script>
 
 <template>
   <main>
-    <section
-      class="xs:px-8 xs:py-12 xs:border border-white/5 flex flex-col items-center justify-center gap-4 xs:bg-white/[0.01] xs:shadow-[0px_0px_25px_0px_rgba(0,0,0,0.5)]"
-    >
-      <header class="flex flex-col items-center justify-center">
-        <img
-          src="/icons/logo.svg"
-          alt=""
-          aria-hidden="true"
-          class="w-48 h-28 mb-4"
-        />
-        <h1 class="tracking-tight text-4xl">Foxford Tools</h1>
-        <p class="text-white/50 text-sm">кастомизация сайта Фокса</p>
+    <section class="card">
+      <header class="card-header">
+        <img src="/icons/logo.svg" alt="" aria-hidden="true" class="logo" />
+        <div>
+          <h1 class="title">Foxford Tools</h1>
+          <p class="subtitle">кастомизация сайта Фокса</p>
+        </div>
       </header>
 
-      <!-- Main links -->
-      <ul
-        class="flex flex-col items-center justify-center w-full [&_li]:w-full gap-2"
-      >
+      <ul class="links">
         <li v-for="link in mainLinks">
-          <Button
-            as="a"
-            class="relative pr-24 overflow-clip min-h-[5.125rem]"
-            :href="link.link"
-          >
-            <div class="flex flex-col justify-center">
-              <span class="text-sm font-normal text-white/50">{{
-                link.subtitle
-              }}</span>
-              <span class="text-[1.75rem]">{{ link.title }}</span>
-            </div>
-
-            <img
-              :src="link.iconSrc"
-              alt=""
-              aria-hidden="true"
-              class="absolute w-[5.25rem] h-fit -right-3 -bottom-3"
-            />
-          </Button>
+          <Link :href="link.link" :title="link.title" :subtitle="link.subtitle" :icon-src="link.iconSrc" />
         </li>
       </ul>
     </section>
   </main>
 </template>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  @media (min-width: 20rem) {
+    padding: 3rem 2rem;
+    border: 1px solid rgb(255 255 255 / 0.05);
+    background-color: rgb(255 255 255 / 0.01);
+    box-shadow: 0px 0px 25px 0px rgb(0 0 0 / 0.5);
+  }
+}
+
+.card-header {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  text-align: center;
+}
+
+.logo {
+  height: 6rem;
+}
+
+.title {
+  font-size: 2.25rem;
+  letter-spacing: -0.025em;
+  font-weight: 500;
+}
+
+.subtitle {
+  font-size: 0.875rem;
+  color: rgb(255 255 255 / 0.5);
+}
+
+.links {
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  gap: 0.5rem;
+}
+
+.links li {
+  width: 100%;
+}
+</style>
