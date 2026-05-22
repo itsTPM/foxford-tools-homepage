d949d89db55b0891e88097f8be53f3f5df21f277
Lyosha <26815728+itsTPM@users.noreply.github.com>
2026-05-22 03:24:27 +0300

    Migrate from Tailwind CSS to Scoped CSS (#1)
    * Migrate from Tailwind CSS to Scoped CSS
    
    * Fix Button type
    
    * Fix v-for key

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -2,17 +2,44 @@
 const props = defineProps({
   as: {
     type: String,
-    default: "button",
+    default: 'button',
   },
 });
 </script>
 
 <template>
-  <component
-    :is="props.as"
-    :type="props.as === 'button' ? button : null"
-    class="p-4 border border-white/5 bg-white/[.02] hover:bg-white/5 transition focus:outline-none focus-visible:ring focus-visible:ring-blue-500 hover:border-white/10 w-full text-lg font-medium flex cursor-pointer select-none"
-  >
+  <component :is="props.as" :type="props.as === 'button' ? 'button' : null" class="button">
     <slot></slot>
   </component>
 </template>
+
+<style scoped>
+.button {
+  padding: 1rem;
+  border: 1px solid rgb(255 255 255 / 0.05);
+  background-color: rgb(255 255 255 / 0.02);
+  width: 100%;
+  font-size: 1.125rem;
+  font-weight: 500;
+  display: flex;
+  cursor: pointer;
+  user-select: none;
+  transition:
+    background-color 150ms ease,
+    border-color 150ms ease;
+  height: 100%;
+
+  &:hover {
+    background-color: rgb(255 255 255 / 0.05);
+    border-color: rgb(255 255 255 / 0.1);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus-visible {
+    box-shadow: 0 0 0 3px rgb(59 130 246 / 0.5);
+  }
+}
+</style>
